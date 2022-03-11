168d5d07cca3910b5281df435729529f364d6ac5
adtjha <adtjha@hotmail.com>
2022-03-11 12:37:28 +0530

    ok

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import fetch from 'node-fetch'
-import cron from 'node-cron'
 import { join, dirname } from 'path'
 import { Low, JSONFile } from 'lowdb'
 import { fileURLToPath } from 'url'
-import * as cheerio from 'cheerio';
+import * as cheerio from 'cheerio'
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+await db.read()
+
+const { raw } = db.data;
 
 const app = express()
 const port = 3000
@@ -13,51 +23,55 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-/**
- * Fetch eci website : https://results.eci.gov.in/ResultAcGenMar2022/statewiseS241.htm?st=S241 every minute for fresh data.
- * Store it in json db.
- */
-
-app.get('/', (req, res) => {
-    fetch('https://results.eci.gov.in/ResultAcGenMar2022/statewiseS241.htm?st=S241').then(response => {
-        response.text().then(body => {
-            const $ = cheerio.load(body);
-            res.send($('#ElectionResult').json())
-        }).catch(err => res.send(err))
-    }).catch(err => res.send(err))
+app.get('/:state/:constituency', (req, res) => {
+    const { state, constituency } = req.params
+    const body = raw[state][constituency]
+    // const $ = cheerio.load(body)
+    res.send(body)
 })
 
-const data = {
-    keys: [],
-    values: []
-}
+// For each state,
+// create a file of state name,
+// For each constituency,
+// fetch roundwise page, with code
+// store in state file
+// Create a singleton writer
 
-for (let i = 0; i <= 41; i++) {
-    fetch(`https://results.eci.gov.in/ResultAcGenMar2022/statewiseS241.htm?st=S24${i}`).then(response => {
-        response.text().then(body => {
-            const $ = cheerio.load(body);
-            $('#ElectionResult').children().each((i, e) => {
-                if (i === 2) {
-                    $(e).children().each((i, e) => {
-                        data.keys.push($(e).text())
-                    })
-                } else if (i >= 4 && i < 13) {
-                    let value = []
-                    $(e).children().each((i, e) => {
-                        if (i === 5 || i === 3) {
-                            let str = $(e).text()
-                            str = str.substring(0, str.indexOf('iParty'))
-                            str = str.trim()
-                            value.push(str)
-                            console.log($(e).text().split('\n')[0])
-                        } else {
-                            value.push($(e).text())
-                        }
-                    })
-                    data.values.push([...value]);
-                }
-            })
-            console.log(data.values)
-        }).catch(console.log)
-    }).catch(console.log)
-}
+// import stateConfig from './src/statesData.config'
+
+// const fetchStateData = async (index) => {
+//     return new Promise(async (resolve, reject) => {
+//         let clist = stateConfig.states[index].constituencyList;
+//         raw[stateConfig.states[index].name] = {}
+//         for (let c = 0; c < clist.length; c++) {
+//             try {
+//                 console.log(`------------------------${stateConfig.states[index].code} - ${clist[c]} - ${c}------------------------`)
+//                 let url = `https://results.eci.gov.in/ResultAcGenMar2022/Roundwise${stateConfig.states[index].code}${clist[c]}.htm?ac=${clist[c]}`
+//                 raw[stateConfig.states[index].name][clist[c]] = {}
+//                 const response = await fetch(url)
+//                 const body = await response.text()
+//                 console.log(body)
+//                 raw[stateConfig.states[index].name][clist[c]] = body
+//                 console.log(`------------------------------------------------`)
+//                 await new Promise((resolve, reject) => setTimeout(() => resolve(), 3000))
+//             } catch (error) {
+//                 reject(error)
+//             }
+//         }
+//         resolve()
+//     })
+// }
+
+// (async () => {
+//     console.log('Start Fetching Data')
+//     for (let i = 0; i <= stateConfig.states.length; i++) {
+//         if (i === 0 || i === 2) {
+//             console.log(`****************************${stateConfig.states[i].name}****************************`)
+//             await fetchStateData(i)
+//             await new Promise((resolve, reject) => setTimeout(() => resolve(), 3000))
+//             console.log(`********************************************************`)
+//         }
+//     }
+//     await db.write()
+//     console.log('Fetching Data Ended')
+// })()
